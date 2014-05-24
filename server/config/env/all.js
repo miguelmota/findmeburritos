a021bae7e14c6d7d9b03d2e44f040454d1fc813e
Miguel Mota <miguelmota2@gmail.com>
2014-05-24 16:09:37 -0700

    port change

--- server/config/env/all.js
+++ server/config/env/all.js
@@ -5,7 +5,7 @@ var rootPath = path.normalize(__dirname + '/../../..');
 
 module.exports = {
 	root: rootPath,
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 3456,
 	hostname: process.env.HOST || process.env.HOSTNAME,
 	db: process.env.MONGOHQ_URL,
 	templateEngine: 'swig',
